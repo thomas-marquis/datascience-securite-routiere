f4207f097bad2e1be47fc65aac584147eda37461
Thomas Marquis <thomas.marquis@ext.cdiscount.com>
2020-12-25 22:50:24 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='secu-route',
+    packages=find_packages(),
+    version='0.1.0',
+    description='analyse et prédction sur les accidents de la route en France',
+    author='thomas-marquis',
+    license='MIT',
+)
