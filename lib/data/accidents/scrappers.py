c9eda46c25967929c4b08c1c2c021243716cf48d
thomas-marquis <38754221+thomas-marquis@users.noreply.github.com>
2020-12-31 11:16:51 +0100

    refactor data cleaning

--- lib/data/accidents/scrappers.py
+++ lib/data/accidents/scrappers.py
@@ -4,24 +4,28 @@ from lib.constant import Datasets
 
 urls = {
     Datasets.CARACS: {
+        2005: 'https://www.data.gouv.fr/fr/datasets/r/a47866f7-ece1-4de8-8d31-3a1b4f477e08',
         2019: 'https://www.data.gouv.fr/fr/datasets/r/e22ba475-45a3-46ac-a0f7-9ca9ed1e283a',
         2018: 'https://www.data.gouv.fr/fr/datasets/r/6eee0852-cbd7-447e-bd70-37c433029405',
         2017: 'https://www.data.gouv.fr/fr/datasets/r/9a7d408b-dd72-4959-ae7d-c854ec505354',
         2016: 'https://www.data.gouv.fr/fr/datasets/r/96aadc9f-0b55-4e9a-a70e-c627ed97e6f7',
     },
     Datasets.LOCATIONS: {
+        2005: 'https://www.data.gouv.fr/fr/datasets/r/3a3488e0-86a1-4917-b082-f3bdc25f6922',
         2019: 'https://www.data.gouv.fr/fr/datasets/r/2ad65965-36a1-4452-9c08-61a6c874e3e6',
         2018: 'https://www.data.gouv.fr/fr/datasets/r/d9d65ca1-16a3-4ea3-b7c8-2412c92b69d9',
         2017: 'https://www.data.gouv.fr/fr/datasets/r/9b76a7b6-3eef-4864-b2da-1834417e305c',
         2016: 'https://www.data.gouv.fr/fr/datasets/r/08b77510-39c4-4761-bf02-19457264790f',
     },
     Datasets.VEHICLES: {
+        2005: 'https://www.data.gouv.fr/fr/datasets/r/924b962b-4400-4468-9f7d-0bdba28f51e9',
         2019: 'https://www.data.gouv.fr/fr/datasets/r/780cd335-5048-4bd6-a841-105b44eb2667',
         2018: 'https://www.data.gouv.fr/fr/datasets/r/b4aaeede-1a80-4d76-8f97-543dad479167',
         2017: 'https://www.data.gouv.fr/fr/datasets/r/d6103d0c-6db5-466f-b724-91cbea521533',
         2016: 'https://www.data.gouv.fr/fr/datasets/r/be2191a6-a7cd-446f-a9fc-8d698688eb9e',
     },
     Datasets.USERS: {
+        2005: 'https://www.data.gouv.fr/fr/datasets/r/cecdbd46-11f2-41fa-b0bd-e6e223de6b3c',
         2019: 'https://www.data.gouv.fr/fr/datasets/r/36b1b7b3-84b4-4901-9163-59ae8a9e3028',
         2018: 'https://www.data.gouv.fr/fr/datasets/r/72b251e1-d5e1-4c46-a1c2-c65f1b26549a',
         2017: 'https://www.data.gouv.fr/fr/datasets/r/07bfe612-0ad9-48ef-92d3-f5466f8465fe',
---
+++ lib/features/cleaning.py
@@ -0,0 +1,117 @@
+import logging
+
+import pandas as pd
+
+
+def clean_caracs_dataset(raw_caracs: pd.DataFrame) -> pd.DataFrame:
+    caracs = raw_caracs.copy()
+
+    # time
+    caracs: pd.DataFrame = pd.concat(
+        [caracs, caracs.loc[:, 'hrmn'].str.extract(
+            r'^(?P<hour>\d{1,2}):?(?P<minute>\d{2})$', expand=False)],
+        axis=1) \
+        .astype({
+        'hour': 'Int32',
+        'minute': 'Int32',
+    })
+
+    # year
+    caracs: pd.DataFrame = caracs.rename(columns={
+        'an': 'year',
+        'jour': 'day',
+        'mois': 'month',
+    })
+    logging.debug(caracs.columns)
+    caracs.loc[caracs['year'].str.len() == 2, 'year'] = '20' + caracs.loc[caracs['year'].str.len() == 2, 'year']
+    caracs.loc[caracs['year'].str.len() == 1, 'year'] = '200' + caracs.loc[caracs['year'].str.len() == 1, 'year']
+    caracs: pd.DataFrame = caracs.astype({'year': 'Int32'})
+
+    # add datetime columns
+    caracs['hour'] = caracs['hour'].fillna(0)
+    caracs['minute'] = caracs['minute'].fillna(0)
+    caracs['datetime'] = pd.to_datetime(
+        caracs.loc[:, ['year', 'month', 'day', 'hour', 'minute']])
+    caracs['weekday'] = caracs['datetime'].dt.weekday
+
+    # remove columns
+    caracs: pd.DataFrame = caracs.drop(['minute', 'gps', 'hrmn'], axis=1)
+
+    return caracs
+
+
+def clean_locations_dataset(raw_locations: pd.DataFrame) -> pd.DataFrame:
+    locations = raw_locations.copy()
+
+    locations = locations.drop(
+        ['v1', 'v2', 'larrout', 'env1'], axis=1)
+    locations['catr'] = locations['catr'].cat.rename_categories({
+        1: 'autoroute',
+        2: 'route_nat',
+        3: 'route_dep',
+        4: 'voie_commu',
+        5: 'no_public',
+        6: 'parking',
+        7: 'metrop_urb',
+        9: 'others',
+    })
+    locations['voie'] = locations['voie'].fillna('')
+    locations['infra'] = locations['infra'].fillna(-1)
+    locations['situ'] = locations['situ'].fillna(-1)
+    locations['lartpc'] = locations['lartpc'] \
+        .fillna('0') \
+        .astype('float32')
+    locations['pr'] = locations['pr'] \
+        .str.replace('(', '').str.replace(')', '') \
+        .astype('float32', errors='ignore')
+    locations['pr1'] = locations['pr1'] \
+        .str.replace('(', '').str.replace(')', '') \
+        .astype('float32', errors='ignore')
+
+    return locations
+
+
+def clean_users_dataset(raw_users: pd.DataFrame) -> pd.DataFrame:
+    users = raw_users.copy()
+
+    users['grav'] = users['grav'].cat.rename_categories({
+        1: 'safe',
+        2: 'killed',
+        3: 'inj_hosp',
+        4: 'inj_light',
+    })
+    users['sexe'] = users['sexe'].cat.rename_categories({
+        1: 'male',
+        2: 'female',
+    })
+    users['catu'] = users['catu'].cat.rename_categories({
+        1: 'driver',
+        2: 'passenger',
+        3: 'pedest',
+        4: 'pedest_roll',
+    })
+    users.loc[users['trajet'] == -1, 'trajet'] = 0
+    users['trajet'] = users['trajet'].cat.rename_categories({
+        0: 'unknown',
+        1: 'home_work',
+        2: 'home_school',
+        3: 'shopping',
+        4: 'pro',
+        5: 'leisure',
+        9: 'other',
+    })
+    users['etatp'] = users['etatp'].fillna(-1)
+    users['locp'] = users['locp'].fillna(-1)
+    users['actp'] = users['actp'].cat.add_categories([-1])
+    users['actp'] = users['actp'].fillna(-1)
+    users = users.drop(['secu', 'id_vehicule', 'secu1', 'secu2', 'secu3'], axis=1)
+
+    return users
+
+
+def clean_vehicles_dataset(raw_vehicles: pd.DataFrame) -> pd.DataFrame:
+    vehicles = raw_vehicles.copy()
+
+    vehicles = vehicles.drop(['id_vehicule', 'motor'], axis=1)
+
+    return vehicles
